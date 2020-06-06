ca8ca2ea4539770b28b7b2201b825db54bb5a0e4
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-06-06 15:32:37 +0530

    Implement GET Articles endpoint

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,13 +1,27 @@
 from flask import Blueprint, abort, jsonify
+from ..models import Article, Comment
 
 API = Blueprint('API', __name__)
 
 # API Error handling
 from . import errors
 
-@API.route('/', methods=["GET"])
 # This endpoint checks api availability
+@API.route('/', methods=["GET"])
 def api_index():
     return jsonify({
         'success': True
+    })
+
+# GET Articles
+# This endpoint fetches all available articles
+@API.route('/articles', methods=["GET"])
+def get_articles():
+
+    articles_data = [item.short() for item in Article.query.all()]
+
+
+    return jsonify({
+        'success': True,
+        'Articles': articles_data
     })
