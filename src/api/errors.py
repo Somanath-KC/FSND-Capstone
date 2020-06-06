65a0bc58a5e240d57bf9a76d820b6b732e3e3ae5
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-06-06 12:35:20 +0530

    Change error handling structure

--- src/api/errors.py
+++ src/api/errors.py
@@ -1,10 +1,27 @@
 from . import API
 from flask import jsonify
 
+
+@API.errorhandler(404)
+def api_resource_not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Resource not found'
+    }), 404
+
+@API.errorhandler(405)
+def api_method_not_allowed(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'Method not allowed'
+    }), 405
+
 @API.errorhandler(500)
-def not_found(error):
+def api_internal_error(error):
     return jsonify({
         'success': False,
         'error': 500,
-        'message': 'API Internal Error'
+        'message': 'API internal error'
     }), 500
