d4f809fed8cd9a287238d302d9829b95f0c072da
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-06-06 13:05:50 +0530

    Add Article, Comment models

--- src/__init__.py
+++ src/__init__.py
@@ -11,12 +11,15 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     app.config.from_pyfile('../config.py')
+
+    # Databse initialization
     db.init_app(app)
-    
-    
+    db.create_all()
+
+    # CORS Configuration
     cors = CORS(app, resources={r"*": {"origin": "*"}})
 
-    # Add cors headers
+    # Add CORS headers
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,17 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Article(db.Model):
+    id = Column(Integer, primary_key=True)
+    author = Column(String(), nullable=False)
+    title = Column(String(), nullable=False)
+    publish_date_time = Column(DateTime,  nullable=False)
+    content = Column(String(),  nullable=False)
+
+class Comment(db.Model):
+    id = Column(Integer, primary_key=True)
+    article_id = Column(Integer, nullable=False)
+    author = Column(String(), nullable=False)
+    content = Column(String(256), nullable=False)
